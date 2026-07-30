578961a630616445016d301db208a46eb4ddeac3
Schaffarczyk <55233361+Schaffarczyk@users.noreply.github.com>
2026-07-30 13:53:59 +0200

    Update echo message for V4 compilation

--- CMD/compVortexCode-V4.cmd
+++ CMD/compVortexCode-V4.cmd
@@ -1,4 +1,4 @@
-echo 'V4 needs mem.f and SubA-V3.f in this directory (copies included)'
+echo 'compilation of V4 '
 gfortran -c -O3 -fno-automatic -fdefault-real-8 mem.f
 gfortran -c -O3 -fno-automatic -fdefault-real-8 VortexCode-V4.f SubA-V3.f
 gfortran mem.o VortexCode-V4.o SubA-V3.o -o VortexCode-V4.exe
